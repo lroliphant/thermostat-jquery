7e9f1bec999a7fc52de280b453bd979afaec1d6d
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2015-07-08 14:44:03 +0100

    Fixed jQuery fcuntion to change colour of temp text on button click event

--- src/thermoController.js
+++ src/thermoController.js
@@ -7,7 +7,9 @@ $("#decreaseTemperatureButton").on("click", decreaseTemperature);
 $("#changePowerStateCheckbox").on("click", changePowerState);
 $("#restartThermostatButton").on("click", restartThermostat);
 
-$("button.changeColour").on("click", energyUse);
+// $("button.changeColour").on("click", energyUse);
+// $("#increaseTemperatureButton").on("click", energyUse);
+$(".changeColour").on("click", energyUse);
 
 function increaseTemperature() {
   thermostat.increase();
@@ -36,25 +38,25 @@ function restartThermostat() {
   showTemperature();
 };
 
-function showTemperature() { 
+function showTemperature() {
   // energyUse();
   document.getElementById("temperature").innerHTML = (thermostat.temperature);
 };
 
 function energyUse() {
-  if(thermostat.changeColor() === 'high') {
-    $('.colour').addClass("high")
+  if( thermostat.changeColor() === 'high') {
+    $('.colour').addClass('high').removeClass('medium');
   }
   if( thermostat.changeColor() === 'medium') {
-    $('.colour').addClass("medium")
+    $('.colour').addClass('medium').removeClass('high low');
+    // $('.colour').addClass('medium').removeClass('low');
   }
   if( thermostat.changeColor() === 'low') {
-    $('.colour').addClass("low")
+      // alert("HELLO")
+    $('.colour').addClass('low').removeClass('medium');
   };
 };
 
 // function energyUse() {
 //   $('#temperature').switchClass( $('#temperature').attr('class'), thermostat.changeColor()) ()
 // };
-
-
